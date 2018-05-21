c968061e6d81f66b4e492ff4d844bb2a9a272d2f
Edwin Mesa <edwinsar.mes@gmail.com>
2018-05-20 19:27:54 -0500

    Pruebas de Commit

--- lesson-1.php
+++ lesson-1.php
@@ -14,24 +14,34 @@ class Person {
 
     var $firstName;
     var $lastName;
-    
-    function __construct($firstName,$lastName) {
+
+    function __construct($firstName, $lastName) {
+        /*
+         * Propiedades de la clase persona
+         */
         $this->firstName = $firstName;
         $this->lastName = $lastName;
     }
-            
+
+    /*
+     * Funciones de la clase persona
+     */
+
     function fullName() {
         return $this->firstName . ' ' . $this->lastName;
     }
 
 }
-
-$person1 = new Person('Edwin','Mesa');
+/*
+ * Instancia de la clase persona, pasando parametros a traves de argumentos 
+ * que se crearon en el constructor.
+ */
+$person1 = new Person('Edwin', 'Mesa');
 //$person1->firstName = 'Edwin';
 //$person1->lastName = 'Sair';
 //exit($person1->fullName());
 
-$person2 = new Person('Sair','Cetina');
+$person2 = new Person('Sair', 'Cetina');
 //$person2->firstName = 'Mesa';
 //$person2->lastName = 'Cetina';
 //exit($person2->fullName());
