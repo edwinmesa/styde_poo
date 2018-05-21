483802c8b10ae1700f35d4504cb773f299c63806
Edwin Mesa <edwinsar.mes@gmail.com>
2018-05-21 17:04:37 -0500

    Inicio del Codigo

--- lesson-2.php
+++ lesson-2.php
@@ -1,2 +1,34 @@
 <?php
 
+class Person {
+
+    var $firstName;
+    var $lastName;
+
+    function __construct($firstName, $lastName) {
+        /*
+         * Propiedades de la clase persona
+         */
+        $this->firstName = $firstName;
+        $this->lastName = $lastName;
+    }
+
+    /*
+     * Funciones de la clase persona
+     */
+
+    function fullName() {
+        return $this->firstName . ' ' . $this->lastName;
+    }
+
+}
+
+$person1 = new Person('Edwin', 'Mesa');
+
+$person2 = new Person('Sair', 'Cetina');
+
+echo "{$person1->fullName()} es amigo de {$person2->fullName()}";
+
+/*
+ * Fin de la clase 2
+ */
