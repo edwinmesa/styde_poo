45e0a86bee7b521075919244b037f2a50909f207
Edwin Mesa <edwinsar.mes@gmail.com>
2018-05-21 21:25:57 -0500

    Fin de Lession 3

--- lesson-3.php
+++ lesson-3.php
@@ -1,8 +1,49 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/*
+ * Incio de la lesson 03
+ * Herencia: Clase extiende de otra.
+ */
+/*
+ * La unidad es algo abstracto, que puede representar muchos objetos
  */
 
+abstract class Unit {
+
+    protected $alive = true;
+    protected $name;
+
+    public function __construct($name) {
+        $this->name = $name;
+    }
+
+    public function move($direction) {
+        echo "<p>{$this->name} camina hacia $direction</p>";
+    }
+
+    abstract public function attack($oponent);
+}
+
+class Soldier extends Unit {
+
+    public function attack($oponent) {
+        echo "<p>{$this->name} corta a $oponent en dos</p>";
+    }
+
+}
+
+class Archer extends Unit {
+
+    public function attack($oponent) {
+        echo "<p>{$this->name} dispara una flecha a $oponent</p>";
+    }
+
+}
+
+$edwin = new Soldier('Sar');
+$edwin->move('Norte');
+$edwin->attack('Bestia');
+
+/*
+ * Fin de lesson -3
+ */
