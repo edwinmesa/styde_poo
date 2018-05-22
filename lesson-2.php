3d6d313e3b40693d153225953d688009a7c28c46
Edwin Mesa <edwinsar.mes@gmail.com>
2018-05-21 20:28:49 -0500

    Final de Lesson 2

--- lesson-2.php
+++ lesson-2.php
@@ -2,32 +2,77 @@
 
 class Person {
 
-    var $firstName;
-    var $lastName;
+    protected $firstname;
+    protected $lastname;
+    protected $nickname;
+    protected $changeNickName = 0;
 
     function __construct($firstName, $lastName) {
-        /*
-         * Propiedades de la clase persona
-         */
         $this->firstName = $firstName;
         $this->lastName = $lastName;
     }
 
-    /*
-     * Funciones de la clase persona
-     */
+    public function setFirstName($firstName) {
+        $this->firstName = $firstName;
+    }
 
-    function fullName() {
+    public function getFirstName() {
+        return $this->firstName;
+    }
+
+    public function setLastName($lastName) {
+        $this->lastName = $lastName;
+    }
+
+    public function getLastName() {
+        return $this->lastName;
+    }
+
+    public function setNickName($nickname) {
+        if (!empty($nickname)) {
+            /*
+             * Si queremos guardar nuestro registro en mayuscula.
+             * y ademas validamos que no este vacio
+             */
+            if ($this->changeNickName >= 3) {
+                /*
+                 * Lanzamiento de exepciones con throw new en caso de repetirse un 
+                 * caso muy seguido. Para este ejemplo le decimos al usuariio que
+                 * no puede cambiar la el nickname mas de tres veces
+                 */
+                throw new Exception("No puedes cambiar el nickname mas de dos veces");
+            }
+            strtoupper($this->nickname = $nickname);
+            $this->changeNickName++;
+        }
+    }
+
+    public function getNickName() {
+        /*
+         * Obtenemos valores en mayuscula
+         */
+        return strtolower($this->nickname);
+    }
+
+    public function fullName() {
         return $this->firstName . ' ' . $this->lastName;
     }
 
 }
 
 $person1 = new Person('Edwin', 'Mesa');
-
 $person2 = new Person('Sair', 'Cetina');
 
-echo "{$person1->fullName()} es amigo de {$person2->fullName()}";
+$person1->setFirstName('Sair');
+$person1->setNickName('dd');
+$person1->setNickName('ddr');
+$person1->setNickName('ddr');
+exit($person1->getNickName());
+
+
+
+
+//echo "{$person1->fullName()} es amigo de {$person2->fullName()}";
 
 /*
  * Fin de la clase 2
