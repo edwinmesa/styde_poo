188373ec6585ba5dc90b228c5c260a67f9a7641c
Edwin Mesa <edwinsar.mes@gmail.com>
2018-06-12 20:50:48 -0500

    Inicio de la Lecion 5

--- lesson-5.php
+++ lesson-5.php
@@ -1,8 +1,101 @@
+
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/*
+ * Incio de la lesson 05
+ * Interfaces y Polimorfismo
+ */
+/*
+ * La unidad es algo abstracto, que puede representar muchos objetos
  */
 
+function show($message) {
+    echo "<p>{$message}</p>";
+}
+
+abstract class Unit {
+
+    protected $alive = true;
+    protected $name;
+    protected $hp = 40;
+
+    public function setHp($points) {
+        $this->hp = $points;
+        show("{$this->getName()} ahora tiene {$this->hp} puntos de vida");
+    }
+
+    public function getHp() {
+        return $this->hp;
+    }
+
+    public function getName() {
+        return $this->name;
+    }
+
+    public function __construct($name) {
+        $this->name = $name;
+    }
+
+    public function move($direction) {
+        show("{$this->getName()} camina hacia $direction");
+    }
+
+    abstract public function attack(Unit $oponent);
+
+    public function takeDamage($damage) {
+        $this->setHp($this->hp - $damage);
+        if ($this->hp <= 0) {
+            $this->dier();
+        }
+    }
+
+    public function dier() {
+        show("{$this->getName()} muere");
+    }
+
+}
+
+class Soldier extends Unit {
+
+    protected $damage = 40;
+
+    public function attack(Unit $oponent) {
+        show("{$this->name} corta a {$oponent->getName()} en dos");
+        $oponent->takeDamage($this->damage);
+    }
+
+    public function takeDamage($damage) {
+        return parent::takeDamage($damage / 2);
+    }
+
+}
+
+class Archer extends Unit {
+
+    protected $damage = 20;
+
+    public function attack(Unit $oponent) {
+        show("{$this->getName()} dispara una flecha a {$oponent->getName()}");
+    
+        $oponent->takeDamage($this->damage);
+    }
+    public function takeDamage($damage) {
+        if(rand(0,1)==1){
+            return parent::takeDamage($damage);
+        }
+    }
+
+}
+
+$sair = new Archer('sar');
+$edwin = new Soldier('edw');
+$edwin->move('Norte');
+//$edwin->attack($sair);
+$sair->attack($edwin);
+$sair->attack($edwin);
+
+$edwin->attack($sair);
+
+/*
+ * Fin de lesson -5
+ */
