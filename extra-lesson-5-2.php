e6f78f6e4ba2cd7859789f39d7def5f07fa0b586
Edwin Mesa <edwinsar.mes@gmail.com>
2018-08-14 21:02:39 -0500

    Finalizacion de Lecion 5

--- extra-lesson-5-2.php
+++ extra-lesson-5-2.php
@@ -19,14 +19,16 @@ abstract class Unit {
     public function getName() {
         return $this->name;
     }
+
     /*
      * Este metodo define los puntos de vida y muestra los puntos
      * Si tiene ataques o con los iniciales
      */
-    private function setHp($points) {
-        $this->hp = $points;
-        show("{$this->name} ahora tiene {$this->hp} puntos de vida");
-    }
+
+//    private function setHp($points) {
+//        $this->hp = $points;
+//        show("{$this->name} ahora tiene {$this->hp} puntos de vida");
+//    }
 
     public function getHp() {
         return $this->hp;
@@ -41,9 +43,16 @@ abstract class Unit {
     }
 
     abstract public function attack(Unit $oponent);
+    /*
+     * Metodo de la clase Unit y el que realiza el daño al oponente
+     * este metodo recibe el daño que el atacante tiene y resta de
+     * asignado. Si el oponente llega a 0 puntos muere y termina
+     * el juego
+     */
 
     public function takeDamage($damage) {
-        $this->setHp($this->hp - $damage);
+        $this->hp = $this->hp - $this->absorbDamage($damage);
+        show("{$this->name} ahora tiene {$this->hp} puntos de vida");
         if ($this->hp <= 0) {
             $this->dier();
         }
@@ -51,6 +60,11 @@ abstract class Unit {
 
     public function dier() {
         show("{$this->name} muere");
+        exit();
+    }
+
+    public function absorbDamage($damage) {
+        return $damage;
     }
 
 }
@@ -59,17 +73,45 @@ class Soldier extends Unit {
 
     protected $damage = 40;
     protected $armor;
-    public function __construct($name, $armor = 2) {
-        $this->armor = $armor;
+
+    public function __construct($name) {
         return parent::__construct($name);
     }
+
+    /*
+     * El SetArmor establece la armadura para el soldado
+     */
+
+    public function setArmor(Armor $armor = null) {
+        $this->armor = $armor;
+    }
+
+    /*
+     * Este metodo attack infringe el daño al oponente
+     */
+
     public function attack(Unit $oponent) {
         show("{$this->name} ataca con la espada a  {$oponent->getName()}");
         $oponent->takeDamage($this->damage);
     }
 
-    public function takeDamage($damage) {
-        return parent::takeDamage($damage / $this->armor);
+    /*
+     * Este metodo recibe el daño pero tiene algo especial
+     * valida si el soldado tiene armadura en caso de tener
+     * el recibe el daño con el absorbdamage a la mitad.
+     */
+
+//    public function takeDamage($damage) {
+//        $damage = $this->absorbDamage($damage);
+//        
+//        return parent::takeDamage($damage);
+//    }
+
+    public function absorbDamage($damage) {
+        if ($this->armor) {
+            $damage = $this->armor->absorbDamage($damage);
+        }
+        return $damage;
     }
 
 }
@@ -83,18 +125,57 @@ class Archer extends Unit {
         $oponent->takeDamage($this->damage);
     }
 
-    public function takeDamage($damage) {
-        if (rand(0, 1) == 1) {
-            return parent::takeDamage($damage);
-        }
+    /*
+     * El arquero recibe el daño pero a veces lo puede
+     * esquivar.
+     */
+//    public function takeDamage($damage) {
+//        if (rand(0, 1) == 1) {
+//            return parent::takeDamage($damage);
+//        }
+//    }
+}
+
+/*
+ * Una interfaz no tiene logica
+ */
+
+interface Armor {
+
+    public function absorbDamage($damage);
+}
+
+class BronzeArmor implements Armor {
+
+    public function absorbDamage($damage) {
+        return $damage / 2;
     }
 
 }
 
-$sar = new Soldier('Bestia',3);
+class SilverArmor implements Armor {
+
+    public function absorbDamage($damage) {
+        return $damage / 3;
+    }
+
+}
+
+class CursedArmor implements Armor {
+
+    public function absorbDamage($damage) {
+        return $damage * 2;
+    }
+
+}
+
+$armor = new BronzeArmor();
+
+$sar = new Soldier('Bestia');
 $edwin = new Archer('Sar');
 $edwin->getHp();
 //$edwin->move('Norte');
 $edwin->attack($sar);
+$sar->setArmor(new CursedArmor());
 $edwin->attack($sar);
 $sar->attack($edwin);
