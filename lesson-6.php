f17d62d2aceaea49181352c716b4b2ac0541a585
Edwin Mesa <edwinsar.mes@gmail.com>
2018-08-20 11:40:59 -0500

    Autocarga y nombres de espacio

--- lesson-6.php
+++ lesson-6.php
@@ -2,18 +2,25 @@
 
 /*
  * Lecion 6 de Styde 
+ * Vamos a usar la autocarga de clases con 
+ * la funcion de PHP, se separa el codigo a 
+ * una carpeta llamada SRC. Tambien se usa los
+ * nombres de espacio. Con el prefijo namespace
+ * para identificar cada clase.
  */
+namespace Styde;
+require './src/helpers.php';
+require './vendor/Armor.php';
 
-function show($message) {
-    echo "<p>{$message}</p>";
-}
-require './src/Unit.php';
-require './src/Soldier.php';
-require './src/Archer.php';
-require './src/Armor.php';
-require './src/BronzeArmor.php';
-require './src/SilverArmor.php';
-require './src/CursedArmor.php';
+spl_autoload_register(function ($className) {
+    if (strpos($className, 'Styde\\') === 0) {
+        $className = str_replace('Styde\\', '', $className);
+        if (file_exists("src/$className.php")) {
+            require "src/$className.php";
+        }
+//exit($className);
+    }
+});
 
 $armor = new BronzeArmor();
 $sar = new Soldier('Bestia');
